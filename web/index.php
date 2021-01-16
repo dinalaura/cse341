70501eb2224d95968678472a4673d39fa825e2c0
dinalaura <dinaguev1@gmail.com>
2021-01-15 20:45:39 -0600

    index

--- web/index.php
+++ web/index.php
@@ -4,13 +4,27 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Homepage - CSE 341</title>
+  <link rel="stylesheet" href="styles.css">
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
+<nav class="navbar navbar-dark bg-dark">
+  <!-- Navbar content -->
+</nav>
+
+<nav class="navbar navbar-dark bg-primary">
+  <!-- Navbar content -->
+</nav>
+
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+  <!-- Navbar content -->
+</nav>
+
+<h1>Dina Moreno - CSE 341</h1>
+
 <?php
-
 // phpinfo();
-
 ?>
-  <h1>Dina Moreno - CSE 341</h1>
 </body>
 </html>
