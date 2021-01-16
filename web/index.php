9941d1db3e111adb98593ced9c30fc891564038b
dinalaura <dinaguev1@gmail.com>
2021-01-15 20:28:10 -0600

    homepage

--- web/index.php
+++ web/index.php
@@ -1,5 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Homepage - CSE 341</title>
+</head>
+<body>
 <?php
 
-  phpinfo();
+// phpinfo();
 
 ?>
+  <h1>Dina Moreno - CSE 341</h1>
+</body>
+</html>
