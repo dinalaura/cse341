397ca7c2855f2fce3a0f19bd586cbf79701b8986
dinalaura <dinaguev1@gmail.com>
2021-01-23 20:31:22 -0600

    pages

--- web/cart.php
+++ web/cart.php
@@ -17,7 +17,7 @@ session_start();
 <!-- nav -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Shopping Cart</a>
+    <a class="navbar-brand" href="https://dina-moreno-cse341.herokuapp.com/cart.php">Shopping Cart</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -27,10 +27,10 @@ session_start();
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">View Cart</a>
+          <a class="nav-link" href="https://dina-moreno-cse341.herokuapp.com/cartview.php">View Cart</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Checkout</a>
+          <a class="nav-link" href="https://dina-moreno-cse341.herokuapp.com/cartcheck.php">Checkout</a>
         </li>
       </ul>
     </div>
---
+++ web/cartcheck.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Checkout</title>
+</head>
+<body>
+<!-- nav -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="https://dina-moreno-cse341.herokuapp.com/cartcheck.php">Checkout</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://dina-moreno-cse341.herokuapp.com/viewcart.php">View Cart</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://dina-moreno-cse341.herokuapp.com/checkout.php">Checkout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- end of nav -->
+</body>
+</html>
